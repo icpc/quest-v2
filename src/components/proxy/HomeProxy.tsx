a39f25a9cd4144cec4089ec39ee6db922709f4b2
Hussien Ibrahiem <hibrahiem@microsoft.com>
2024-12-06 10:00:58 +0200

    Fix icon login in header & enhance no data found messages

--- src/components/proxy/HomeProxy.tsx
+++ src/components/proxy/HomeProxy.tsx
@@ -1,33 +1,54 @@
 import React from "react";
 import Home from "../Home";
-import {getQuests} from "../../utils/requests";
-import Loader, {LoaderComponent} from "./Loader";
-import {UserInfoProps} from "../../types/types";
+import { getQuests } from "../../utils/requests";
+import Loader, { LoaderComponent } from "./Loader";
+import { UserInfoProps } from "../../types/types";
 
-const HomeProxyHelper: React.FC<UserInfoProps> = ({userInfo}) => {
-    const [quests, setQuests] = React.useState([]);
-    const [isQuestsLoading, setIsQuestsLoading] = React.useState(true);
+const HomeProxyHelper: React.FC<UserInfoProps> = ({ userInfo }) => {
+  const [quests, setQuests] = React.useState([]);
+  const [isQuestsLoading, setIsQuestsLoading] = React.useState(true);
 
-    React.useEffect(() => {
-        if (userInfo) {
-            getQuests(userInfo).then((response) => {
-                setQuests(response?.quests);
-                setIsQuestsLoading(false);
-            });
-        }
-    }, [userInfo]);
-
-    if (isQuestsLoading) {
-        return <LoaderComponent />
+  React.useEffect(() => {
+    if (userInfo) {
+      getQuests(userInfo).then((response) => {
+        setQuests(response?.quests);
+        setIsQuestsLoading(false);
+      });
     }
-    if (!quests || quests.length === 0) {
-        return <div>No quests found</div>;
-    }
-    return <Home userInfo={userInfo} quests={quests}/>;
+  }, [userInfo]);
+
+  if (isQuestsLoading) {
+    return <LoaderComponent />;
+  }
+  if (!quests || quests.length === 0) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          textAlign: "center",
+          backgroundColor: "#f8f9fa",
+          color: "#343a40",
+          fontFamily: "Arial, sans-serif",
+          fontSize: "1.5rem",
+          border: "1px solid #ddd",
+          borderRadius: "8px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+          padding: "20px",
+        }}
+      >
+        <p>🚀 Quests will be available soon!</p>
+      </div>
+    );
+  }
+  return <Home userInfo={userInfo} quests={quests} />;
 };
 
 const HomeProxy = () => {
-    return <Loader component={HomeProxyHelper}/>
+  return <Loader component={HomeProxyHelper} />;
 };
 
 export default HomeProxy;

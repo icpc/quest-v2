365441d94aa9f36ac604043638936acca9dac088
Azat Ismagilov <azatismagilov00@gmail.com>
2025-08-24 14:44:05 +0100

    refactor(auth): remove unused login form and related state management

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import { useNavigate } from "react-router";
 
-import { Link } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
 
-import { checkAuth, login, loginOIDC } from "../utils/requests";
+import { checkAuth, loginOIDC } from "../utils/requests";
 
 const defaultTheme = createTheme();
 
@@ -24,28 +22,12 @@ const LoginFormWrapper = styled(Box)(({ theme }) => ({
 export default function SignIn() {
   const navigate = useNavigate();
   const isAuthenticated = checkAuth();
-  const [isLoginLoading, setIsLoginLoading] = React.useState(false);
 
   React.useEffect(() => {
     if (isAuthenticated) {
       navigate("/home");
     }
   }, [isAuthenticated, navigate]);
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setIsLoginLoading(true);
-    const data = new FormData(event.currentTarget);
-    const email = data.get("email");
-    const password = data.get("password");
-    const newLogin = await login({ email, password });
-    if (newLogin !== null) {
-      navigate("/home");
-    } else {
-      alert("Invalid credentials please fill the form to get your password");
-    }
-    setIsLoginLoading(false);
-  };
 
   return (
     <ThemeProvider theme={defaultTheme}>

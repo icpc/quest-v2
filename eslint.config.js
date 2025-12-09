2a0f78bc2a44c2e68501e2e541975adab4c792f8
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-09 22:41:45 +0000

    Fix eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { ignores: ["pocketbase/**"] },
+  includeIgnoreFile(
+    fileURLToPath(new URL(".gitignore", import.meta.url)),
+    "Imported .gitignore patterns",
+  ),
+  includeIgnoreFile(
+    fileURLToPath(new URL(".prettierignore", import.meta.url)),
+    "Imported .prettierignore patterns",
+  ),
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     plugins: { js },

07c4ab58d203993ad0074561f35b3f646f03dc01
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-19 15:21:22 +0000

    Install tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),

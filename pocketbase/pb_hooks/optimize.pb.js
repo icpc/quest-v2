da315f9428e8e9cce4fd9b61ced48f0ee5d60f6f
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-27 19:55:19 +0000

    Remove unnecessary onAfterBootstrap wrapper from sqlite optimization hook

--- pocketbase/pb_hooks/optimize.pb.js
+++ pocketbase/pb_hooks/optimize.pb.js
@@ -1,12 +1,10 @@
 /// <reference path="../pb_data/types.d.ts" />
 
-onAfterBootstrap(() => {
-  cronAdd("sqlite_optimize", "0 * * * *", () => {
-    try {
-      $app.db().newQuery("PRAGMA optimize").execute();
-      console.log("[sqlite_optimize] done");
-    } catch (e) {
-      console.log("[sqlite_optimize] error:", String(e));
-    }
-  });
+cronAdd("sqlite_optimize", "0 * * * *", () => {
+  try {
+    $app.db().newQuery("PRAGMA optimize").execute();
+    console.log("[sqlite_optimize] done");
+  } catch (e) {
+    console.log("[sqlite_optimize] error:", String(e));
+  }
 });

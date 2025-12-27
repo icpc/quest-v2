028347850286051ac32a4cae4060301dce5d0ad3
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-27 19:44:22 +0000

    Refactor optimize hook to include error handling and logging

--- pocketbase/pb_hooks/optimize.pb.js
+++ pocketbase/pb_hooks/optimize.pb.js
@@ -1,11 +1,12 @@
 /// <reference path="../pb_data/types.d.ts" />
 
-// run after app start
 onAfterBootstrap(() => {
-  $app.dao().db().newQuery("PRAGMA optimize").execute();
-});
-
-// run every hour
-cronAdd("pragma_optimize", "0 * * * *", () => {
-  $app.dao().db().newQuery("PRAGMA optimize").execute();
+  cronAdd("sqlite_optimize", "0 * * * *", () => {
+    try {
+      $app.db().newQuery("PRAGMA optimize").execute();
+      console.log("[sqlite_optimize] done");
+    } catch (e) {
+      console.log("[sqlite_optimize] error:", String(e));
+    }
+  });
 });

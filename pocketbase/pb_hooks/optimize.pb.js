402e2f417e20dfdb29a745946851348dd425b8c4
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-27 20:26:43 +0000

    Run analyze every day instead

--- pocketbase/pb_hooks/optimize.pb.js
+++ pocketbase/pb_hooks/optimize.pb.js
@@ -1,8 +1,8 @@
 /// <reference path="../pb_data/types.d.ts" />
 
-cronAdd("sqlite_optimize", "0 */2 * * *", () => {
+cronAdd("sqlite_optimize", "0 0 * * *", () => {
   try {
-    $app.db().newQuery("PRAGMA optimize=0x10002").execute();
+    $app.db().newQuery("ANALYZE; PRAGMA optimize;").execute();
     console.log("[sqlite_optimize] with 0x10002 done");
   } catch (e) {
     console.log("[sqlite_optimize] error:", String(e));
